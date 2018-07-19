e2295bc6379857fd21b2c2544c3ee260c8e5cd46
Gabriele Turelli <cappy95@gmail.com>
2018-07-19 16:19:44 +0200

    Actually declare the right license

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License 3",
         "Operating System :: OS Independent",
     ),
 )
